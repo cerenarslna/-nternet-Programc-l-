ebb44b2532c9547a4e9d9fca8310298278e6e4e2
Ceren Arslan <ceren.arslan2@ogr.gelisim.edu.tr>
2024-10-18 11:37:52 +0300

    Add files via upload

---
+++ hafta3.php
@@ -0,0 +1,72 @@
+<?php
+/*echo "isset($AdiSoyadi)";
+$CerenArslan="Ceren Arslan";
+echo "isset($AdiSoyadi)";
+*/
+
+
+
+
+
+//İsset, unset kullanımı 
+/*echo "isset($AdiSoyadi)"."<br>";
+$AdiSoyadi="Ceren Arslan";
+echo "isset($AdiSoyadi)"."<br>";
+
+$degisken="Gelişim Üniversitesi";
+echo $degisken."<br>";
+unset($degisken);
+echo $degisken;
+*/
+
+
+
+
+
+ /* //Doğum yılı hesaplama
+$yil=2024;
+$dogum_yili=2002;
+$yas=$yil-$dogum_yili;
+echo "benim yaşım".$yas;
+*/
+
+
+
+
+/*//Basit Hesap Makinesı
+echo "<h3>Basit Hesap Makinesi</h3>"."<br>";
+$sayi1=35;
+$sayi2=5;
+
+$topla=$sayi1+$sayi2;
+$cikar=$sayi1-$sayi2;
+$bol=$sayi1/$sayi2;
+$mod=$sayi1%$sayi2;
+$us=$sayi1**$sayi2;
+
+echo "1.sayının değeri:" .$sayi1."<br>". 
+     "2.sayının değeri:" .$sayi2."<br>".  
+     "Toplama işleminin sonucu:".$topla."<br>". 
+     "Çıkarma işleminin sonucu:".$cikar."<br>". 
+     "Çarpma işleminin sonucu:".$carp."<br>". 
+     "Mod alma  işleminin sonucu:".$mod."<br>". 
+     "Us işleminin sonucu:".$us;
+ 
+*/
+
+
+ /*//Arttırma ve Azaltma Operatorlerı
+ $sayi=15;
+echo "<u>Yazmış olduğunuz sayının değeri: </u>".$sayi."<br>". 
+"Sayının değerini arttırma işlemi: " .++$sayi."<br>".
+"Sayının değerini azaltma işlemi: " .--$sayi."<br>. 
+*/
+
+
+
+
+
+
+
+
+?>
---
+++ hafta3html.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kullanıcı Formu</title>
+</head>
+<body>
+    <form name="form" action=" " method="post">
+        <table>
+<tr>
+<td>Adı:</td>
+<td><input type="text" name="adi">
+</td>
+</tr>
+
+<tr>
+<td>Soyadı:</td>
+<td><input type="text" name="soyadi"></td>
+</tr>
+
+<tr>
+    <td><input type="submit" name="gonder" value="Kaydet"></td>
+</tr>
+        </table>
+</body>
+</html>
+
+<?php
+if(isset($_POST["gonder"]))
+{
+$kadi=$_POST["adi"];
+$ksoyadi=$_POST["soyadi"];
+echo "Merhaba" .$kadi. " ".$ksoyadi;
+
+
+}
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
