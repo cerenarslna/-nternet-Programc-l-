8d961a99d50884df83870b979d5b348d0141caa0
Ceren Arslan <ceren.arslan2@ogr.gelisim.edu.tr>
2024-11-09 03:00:05 +0300

    Add files via upload

---
+++ hafta6.php
@@ -0,0 +1,90 @@
+<?php 
+/* $sayi=1;
+while($sayi<10)
+{
+    echo "Sayi: ".$sayi."<br>";
+    $sayi++;
+}
+
+*/
+
+/*$sayi=1;
+echo $sayi;
+$toplam=$sayi;
+while($sayi<10)
+{
+    $sayi++;
+    $toplam+=$sayi;
+    echo "+".$sayi;
+}
+echo "=".$toplam;
+*/
+
+
+
+//Do-While 
+
+/*$sayi=1;
+do 
+{
+    echo $sayi."-Ceren Arslan"."<br>";
+    $sayi++;
+}
+while ($sayi<=5);
+*/
+
+/*
+$sayi=30;
+$a=1;
+do
+{
+    if($sayi%$a==0)
+    {
+        echo $a."sayısı".$sayi."sayısının bir bölenidir"."<br>";
+    }
+    $a++;
+}
+while($a<=$sayi);
+
+*/
+
+//Foreach dongusu
+
+/*
+$gunler=array("Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar");
+foreach($gunler as $gun)
+{
+    echo $gun."<br>";
+}
+*/
+/*
+$KalınveInceUnluler=array("a","ı","o","u","e","i","ö","ü");
+echo "Alfabedeki Kalın ve İnce Ünlüler Listesi"."<br>";
+foreach($KalınveInceUnluler as $alfabe=>$sira)
+{
+    echo $alfabe." : ".$sira."<br>";
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
