59a7a85ccc8f47f8b522136ee390385d3d37e2b4
Ceren Arslan <ceren.arslan2@ogr.gelisim.edu.tr>
2024-11-18 01:37:45 +0300

    Add files via upload

---
+++ hafta7.php
@@ -0,0 +1,28 @@
+<?php
+/*
+//Kücükten Büyüge Tekrarlayan isim 
+
+for ($i=1;$i<=7;$i++)
+{
+    echo "<font size=".$i."<b>CEREN ARSLAN</b>"."<br>"."</font>";
+}
+    */
+/*
+    // 1 den 10 a kadar sayıların karesi
+    for ($i=1;$i<=10;$i++)
+    {
+        //echo $i." sayısının karesi: ".$i*$i."<br>";
+        //echo $i." sayısının karesi: ".($i**2)."<br>";
+        echo $i." sayısının karesi: ".pow($i,2)."<br>";
+    }
+*/
+
+
+
+
+
+
+
+
+
+?>
